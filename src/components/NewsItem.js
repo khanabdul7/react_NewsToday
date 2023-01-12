fe8a1be11ffd8eb2acded16955d6440b47ba9aa4
Abdul khan <abdulmatania@gmail.com>
2023-01-12 22:45:07 +0530

    day2 Added api calls (bug- data is not getting displayed)

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,9 +7,9 @@ export default class NewsItem extends Component {
             <div className="card" style={{ width: "18rem" }}>
                 <img src={imageUrl} className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    <p className="card-text">{desc}</p>
-                    <a href={newsUrl} className="btn-sm btn-primary">Read More</a>
+                    <h5 className="card-title">{title}...</h5>
+                    <p className="card-text">{desc}...</p>
+                    <a href={newsUrl} target="_blank" rel="noreferrer" className="btn-sm btn-primary">Read More</a>
                 </div>
             </div>
         </>
