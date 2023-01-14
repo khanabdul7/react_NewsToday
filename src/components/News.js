d91a6d65653bb10bde7ef5741306ea3122e36107
Abdul khan <abdulmatania@gmail.com>
2023-01-14 12:35:05 +0530

    day3 Added pagination concept

--- src/components/News.js
+++ src/components/News.js
@@ -7,21 +7,49 @@ export default class News extends Component {
         super();
         console.log("Constructor inside News component")
         this.state = {
-            articles: []
+            articles: [],
+            page: 1,
+            totalResults: 0
         }
     }
 
     async componentDidMount(){
-        let url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=b04ef152ebfc458ab8d05f0aa8c6bdc0";
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=b04ef152ebfc458ab8d05f0aa8c6bdc0&page=1&pageSize=9`;
         let data = await fetch(url);
         let processedData = await data.json();
         console.log(processedData)
-        this.setState=({
-            articles: processedData.articles
+        this.setState({
+            articles: processedData.articles,
+            page: this.state.page,
+            totalResults: processedData.totalResults
         })
-        console.log("after setting")
     }
     // why data is not showing in News component ? maybe render is not working after getting data
+    // solved : issue was in setstate method.
+
+    handlePrevbtn = async () => {
+        console.log("Prevbtn")
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=b04ef152ebfc458ab8d05f0aa8c6bdc0&page=${this.state.page - 1}&pageSize=9`;
+        let data = await fetch(url);
+        let processedData = await data.json();
+        console.log(processedData)
+        this.setState({
+            page: this.state.page - 1,
+            articles: processedData.articles
+        })
+    }
+
+    handleNextbtn = async () => {
+        console.log("Nextbtn")  
+            let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=b04ef152ebfc458ab8d05f0aa8c6bdc0&page=${this.state.page + 1}&pageSize=9`;
+            let data = await fetch(url);
+            let processedData = await data.json();
+            console.log(processedData)
+            this.setState({
+                articles: processedData.articles,
+                page: this.state.page + 1           
+            })            
+    }
 
     render() {
         return (
@@ -34,8 +62,11 @@ export default class News extends Component {
                     </div>
                 })}
                 </div>
+                <div className="d-flex justify-content-between">
+                <button disabled={this.state.page<=1} type="button" onClick={this.handlePrevbtn} className="btn btn-dark">&larr; Previous</button>
+                <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/9)} type="button" onClick={this.handleNextbtn} className="btn btn-dark">Next &rarr;</button>
+                </div>
             </div>
-
         )
     }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,13 +3,14 @@ import React, { Component } from 'react'
 export default class NewsItem extends Component {
     render() {
         let {title, desc, imageUrl, newsUrl} = this.props
-        return (<>
+        return (
+        <>
             <div className="card" style={{ width: "18rem" }}>
                 <img src={imageUrl} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">{title}...</h5>
                     <p className="card-text">{desc}...</p>
-                    <a href={newsUrl} target="_blank" rel="noreferrer" className="btn-sm btn-primary">Read More</a>
+                    <a href={newsUrl} target="_blank" rel="noreferrer" className="btn-sm btn-dark">Read More</a>
                 </div>
             </div>
         </>
