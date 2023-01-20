d432195cc6a8733ffa5eb6b1b37482af5360f129
Abdul khan <abdulmatania@gmail.com>
2023-01-20 21:45:26 +0530

    day6 Changed Components Type

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import defaultImg from '../img/defaultNews.jpeg'
 
-export default class NewsItem extends Component {
-    render() {
-        let { title, desc, imageUrl, newsUrl, publishedAt, author, source } = this.props
-        return (
-            <>
-                <div className="card">
-                    <img src={!imageUrl ? defaultImg : imageUrl} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
-                        <p className="card-text">{desc}...</p>
-                        <p className="card-text"><small className="text-muted"><span className="badge bg-secondary">Source: {source}</span> </small></p>
-                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(publishedAt).toUTCString()}</small></p>
-                        <a href={newsUrl} target="_blank" rel="noreferrer" className="btn-sm btn-dark">Read More</a>
-                    </div>
+const NewsItem = (props) => {
+    let { title, desc, imageUrl, newsUrl, publishedAt, author, source } = props
+    return (
+        <>
+            <div className="card">
+                <img src={!imageUrl ? defaultImg : imageUrl} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}...</h5>
+                    <p className="card-text">{desc}...</p>
+                    <p className="card-text"><small className="text-muted"><span className="badge bg-secondary">Source: {source}</span> </small></p>
+                    <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(publishedAt).toUTCString()}</small></p>
+                    <a href={newsUrl} target="_blank" rel="noreferrer" className="btn-sm btn-dark">Read More</a>
                 </div>
-            </>
-        )
-    }
+            </div>
+        </>
+    )
 }
+
+export default NewsItem;
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Loading from '../img/Loading.gif'
 
-export default class Spinner extends Component {
-  render() {
-    return (
-        <div className="text-center">
-            <img src={Loading} alt="Loading"/>
-        </div>
-    )
-  }
+const Spinner = () => {
+  return (
+    <div className="text-center">
+      <img src={Loading} alt="Loading" />
+    </div>
+  )
 }
+
+export default Spinner;
